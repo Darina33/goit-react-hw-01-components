7bb074893fdde3a8477b6b6a3a4e22724d894640
Darina33 <115955389+Darina33@users.noreply.github.com>
2023-05-20 16:30:18 +0300

    title

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,9 +4,14 @@ import css from './Statistics.module.css';
 import color from '../utils/RandomColor';
 
 
+      
+
+
 export default function Statistics({ title, stats }) {
   return (<section className={css.statistics}>
-  <h2 className={css.title}>{title}</h2>
+  {title && (<h2 className={css.title}>{title}</h2>
+)
+}
     <ul className={css.stat_list}>
             {stats.map(({ id, label, percentage }) => 
             {
